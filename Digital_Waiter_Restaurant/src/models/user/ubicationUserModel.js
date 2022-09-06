a08676fbbc3a8f6da04e391a2050e1817af3ee99
Robin-Rivadeneira <rivadeneirarobin@gmail.com>
2022-09-06 11:37:10 -0500

    ubicationUser

---
+++ Digital_Waiter_Restaurant/src/models/user/ubicationUserModel.js
@@ -0,0 +1,29 @@
+const ubicationUser = (sequelize, type)=>{
+    return sequelize.define('ubicationUsers', {
+        idUbicationUser: {
+            type: type.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        mainstreetLocationUser: type.STRING,
+        streetSecondaryLocationUser: type.STRING,
+        referenceLocationUser: type.STRING,
+        HouseNumberLocationUsername: type.STRING,
+        creationUbicationUsers: {
+            type: "TIMESTAMP",
+            defaultValue: type.literal("CURRENT_TIMESTAMP"),
+            allowNull: false,
+        },
+        updateUbicationUsers: {
+            type: "TIMESTAMP",
+            defaultValue: type.literal("CURRENT_TIMESTAMP "),
+            allowNull: false,
+        },
+    },
+    {
+        timestamps: false,
+    }
+);
+};
+
+module.exports = ubicationUser;
